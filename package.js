fd24bb83899fd4ed545f7ec59b6809e9b2a9e3a1
Thomas Steinhauer <thomassteinhauer@gmail.com>
2015-02-26 00:35:34 +0100

    description

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
-    summary: "String.jsA dead simple, customizable plain text parser for JS",
-    version: "1.0.0",
-    git: " \* Fill me in! *\ "
+    summary: "String.js - A dead simple, customizable plain text parser for JS - ported to meteor",
+    version: "2.1.0",
+    git: "https://github.com/strikeout/meteor-string.js"
 });
 
 Package.onUse(function (api) {
     api.versionsFrom('METEOR@0.9.3');
     api.use('underscore');
-    api.addFiles('lib/string.js', 'client');
+    api.addFiles('lib/string.js', ['client','server']);
 });
